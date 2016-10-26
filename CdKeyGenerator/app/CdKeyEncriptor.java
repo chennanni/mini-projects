9a637e4c9ca94ada6081eaf2e35035ccefad76b2
Chennan <chennancd@gmail.com>
2016-10-26 11:38:16 -0400

    Create CdKeyEncriptor.java

---
+++ CdKeyGenerator/app/CdKeyEncriptor.java
@@ -0,0 +1,50 @@
+package app.cd_key_generator;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+// download external jar from http://commons.apache.org/proper/commons-codec/download_codec.cgi
+
+public class CdKeyEncriptor {
+	
+	static private String key = "Bar12345Bar12345";  // 128 bit key
+	static private String initVector = "RandomInitVector";  // 16 bytes IV
+	
+	// credit for this two methods from 'chandpriyankara' of StackOverFlow
+	public static String encrypt(String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            //System.out.println("encrypted string: " + Base64.encodeBase64String(encrypted));
+
+            return Base64.encodeBase64String(encrypted);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
