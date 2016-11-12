25c47f30531d9edf66d5409bedad0eb50612de3f
Chennan <chennancd@gmail.com>
2016-11-12 17:31:56 -0500

    Create Constants.java

---
+++ ProgressBar/src/app.progress_bar/Constants.java
@@ -0,0 +1,8 @@
+package app.progress_bar;
+
+public class Constants {
+	static final int MAX_TASK_NUM = 5;
+	static final int WINDOW_WIDTH = 300;
+	static final int WINDOW_HEIGHT = 100;
+	static final int TASK_HEIGHT = 30;
+}
