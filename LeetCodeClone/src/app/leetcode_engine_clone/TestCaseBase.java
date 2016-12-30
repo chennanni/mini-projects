666d3c018a684c68bb996ed68e435f84af129db6
Chennan <chennancd@gmail.com>
2016-12-30 11:09:53 -0500

    Create TestCaseBase.java

---
+++ LeetCodeClone/src/app/leetcode_engine_clone/TestCaseBase.java
@@ -0,0 +1,35 @@
+package app.leetcode_engine_clone;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+public class TestCaseBase {
+	
+	static SolutionInterface solution;
+	
+	public static SolutionInterface getSolution() {
+		return solution;
+	}
+	
+	@BeforeClass
+	public static void setUp() {
+		solution = (SolutionInterface) RunTimeComplier.loadAndCompile();
+	}
+	
+	@Test
+	public void testImplementSolutionInterface() {
+        try {
+            Class<?> userImplementedClass = solution.getClass();
+            Method userImplementedMethod = userImplementedClass.getMethod("reverseString", String.class); // method name + method parameter
+            Class<?> userImplementedReturnType = userImplementedMethod.getReturnType();
+            assertTrue("method returns the wrong type.", userImplementedReturnType.equals(String.class));
+        } catch (NoSuchMethodException | SecurityException e) {
+            fail("method is missing.");
+        }
+	}
+	
+}
