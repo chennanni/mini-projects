abef574a3151446040108b69c840002af3b4d42e
Chennan <chennancd@gmail.com>
2018-07-17 14:26:27 +0800

    Update app.py

--- WallpaperChanger/app.py
+++ WallpaperChanger/app.py
@@ -10,14 +10,14 @@ folder_path_ngt = r'C:\Users\Administrator.WIN-1J9R69V4VDR\Desktop\bg\night\\'
 
 # https://blog.csdn.net/felixzh123/article/details/76085132
 def set_wallpaper(img_path):
-    # 打开指定注册表路径
-    reg_key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
-    # 最后的参数:2拉伸,0居中,6适应,10填充,0平铺
-    win32api.RegSetValueEx(reg_key, "WallpaperStyle", 0, win32con.REG_SZ, "6")
-    # 最后的参数:1表示平铺,拉伸居中等都是0
-    win32api.RegSetValueEx(reg_key, "TileWallpaper", 0, win32con.REG_SZ, "0")
-    # 刷新桌面
-    win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, img_path, win32con.SPIF_SENDWININICHANGE)
+	# 打开指定注册表路径
+	reg_key = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER, "Control Panel\\Desktop", 0, win32con.KEY_SET_VALUE)
+	# 最后的参数:2拉伸,0居中,6适应,10填充,0平铺
+	win32api.RegSetValueEx(reg_key, "WallpaperStyle", 0, win32con.REG_SZ, "6")
+	# 最后的参数:1表示平铺,拉伸居中等都是0
+	win32api.RegSetValueEx(reg_key, "TileWallpaper", 0, win32con.REG_SZ, "0")
+	# 刷新桌面
+	win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, img_path, win32con.SPIF_SENDWININICHANGE)
 
 if __name__ == '__main__':
 	# get current hour
